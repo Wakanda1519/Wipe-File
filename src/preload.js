df24177cfae40a3ff4bca890c1f701c691a025d1
wakanda <dvornikov-2005@inbox.ru>
2026-01-13 22:51:35 +0500

    Update preload.js

--- src/preload.js
+++ src/preload.js
@@ -1,13 +1,10 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('api', {
-  // Файловые операции
   selectFile: () => ipcRenderer.invoke('select-file'),
   selectDirectory: () => ipcRenderer.invoke('select-directory'),
   deleteFile: (filePath) => ipcRenderer.invoke('delete-file', filePath),
   deleteFilesInDirectory: (params) => ipcRenderer.invoke('delete-files-in-directory', params),
-  
-  // Операции с хранилищем
   getOperations: () => ipcRenderer.invoke('get-operations'),
   saveOperations: (operations) => ipcRenderer.invoke('save-operations', operations),
   getLogs: (operationId) => ipcRenderer.invoke('get-logs', operationId),
